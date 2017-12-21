5a4ba83fb8943c886461e8b0921532c35bc9f887
xYue <yuelc@outlook.com>
2017-12-21 14:27:57 +0800

    first

---
+++ CoreApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        [Route("[action]")]
+        public UserModel QueryUser(int id =3)
+        {
+            var user = new UserModel()
+            {
+                Id = id,
+                Name = "大神" + id,
+                Sex = Convert.ToBoolean(id % 2)
+            };
+            return user;
+        }
+
+        [Route("GetUser")]
+        public UserModel QueryUser([FromBody] UserModel um)
+        {
+            //var user = new UserModel()
+            //{
+            //    Id = id,
+            //    Name = "大神" + id,
+            //    Sex = Convert.ToBoolean(id % 2)
+            //};
+            return um;
+        }
+
+    }
+}
